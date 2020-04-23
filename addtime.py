361a59eb8f81aab723d5fd58967f3fe67e4188b7
ABertoni <andresibertoni@gmail.com>
2020-04-23 18:15:53 -0300

    New version (2020): the .log file doesn't have to be clean or as one whole part (sharing only one header).

--- addtime.py
+++ addtime.py
@@ -9,7 +9,7 @@ print("""
 
 ### Add time info to files with step info, using information located in a .log file
 #### The .log file must have a header in the first line (where "Step" and "Time" columns are present) and the information below will be used for conversion of steps to times. Duplicated steps entries will be overwriten.
-#### In the files to process the step column has to be present and a header is not required. The index of the column where step info is located has to be inputed as a the 'sinfo' variable. But, if the program fails to locate it, it will try to match the info in the other columns using the conversion dictionary (not recommended).
+#### In the files to process the step column has to be present and a header is not required. The index of the column where step info is located has to be inputed as a the 'sinfo' variable.
 #### The user also has to add in 'filexs' (as elements in the list) the formats of the files that will be processed. Files with the "vs-t" tag will be ignored.
 
 # Import modules
@@ -39,34 +39,31 @@ try:
 except:
     if len(logfiles)==0: print(".log file NOT found!")
     sys.exit()
-        
-# Read location of step and time columns
-firstline = open(logfile,"r").readline()
-firstcol = firstline.split()[0].strip()
-if firstcol[0].isalpha():
-    for idx,col in enumerate(firstline.split()):
-        if col == "Step": idxstep = idx
-        elif col == "Time": idxtime = idx
-else:
-    print("Header in first line NOT found!")
-    sys.exit()
-    
-# Check if step and time info exists
-try: 
-    if idxstep != None and idxtime != None:
-        print("Step and time info located!")
-except:
-    print("Step and time info not found in the .log file!")
-    
-# Build step to time conversion dictionary
-convert = {}
-logdata = np.genfromtxt(logfile)
 
-nlines = logdata[:,0].size
-for n in range(nlines):
-    step = logdata[n,idxstep]
-    time = logdata[n,idxtime]
-    convert[step] = time
+# Extracts data sections
+with open(logfile,"r") as logfile:
+    loglines = logfile.readlines()
+
+    headers_sta_idx = []
+    for idx,line in enumerate(loglines):
+        if "Step" in line and "Time" in line: headers_sta_idx.append(idx)
+    if len(headers_sta_idx) != 0: print("Step and time info located!")
+    else: print("Step and time info not found in the .log file!")
+
+    convert = {}
+    for sta_idx in headers_sta_idx:
+        header = loglines[sta_idx]
+        for idx,col in enumerate(header.split()):
+            if col == "Step": idxstep = idx
+            elif col == "Time": idxtime = idx
+
+        for line in loglines[sta_idx+1:]:
+            columns = line.split()
+            if any([item.isalpha() for item in columns]): break
+            else:
+                step = float(columns[idxstep])
+                time = float(columns[idxtime])
+                convert[step] = time
 
 ####	####	####
 
@@ -74,7 +71,6 @@ for n in range(nlines):
 files = [f for f in os.listdir(cwd) if f.split(".")[-1] in filexs and "vs-t" not in f]
 
 for file in files:
-    stepinfo = sinfo
     print("\nProcessing %s file..."%file)
     filedata = np.genfromtxt(file)
     nlines, ncols = filedata.shape
@@ -83,26 +79,15 @@ for file in files:
     # Check if step info is correct
     def checkinfo(): # Uses global varibles
         for n in range(nlines):
-            step = filedata[n,stepinfo]
+            step = filedata[n,sinfo]
             try: convert[step]
             except: return False
         return True
             
-    if not checkinfo():
-        print("Step info not found where indicated...\n\t(Looking for an alternative)")
-        for nc in range(ncols):
-            stepinfo = nc
-            if checkinfo(): 
-                print("\t(Step info found in column index %s)"%stepinfo)
-                break
-            stepinfo = None
-            
-    if stepinfo == None:
-        print("Step info not found at all!\nSkiping this file!\n")
-        break
-    
+    if not checkinfo(): print("Step info not found where indicated...\n (file: %s)"%file)
+
     for n in range(nlines):
-        step = filedata[n,stepinfo]
+        step = filedata[n,sinfo]
         data = filedata[n,:]
         newdata[n,0] = convert[step]
         newdata[n,1:] = data
@@ -115,7 +100,7 @@ print("\nAll done!")
 print("\n~~~~~~~\nAndrés Ignacio Bertoni (andresibertoni@gmail.com) - 2019\n~~~~~~~")
 
 ################################################################################
-# Andrés Ignacio Bertoni (andresibertoni@gmail.com) - 2019                     #
+# Andrés Ignacio Bertoni (andresibertoni@gmail.com) - 2020                     #
 # Quantum Dynamics Group - Interdisciplinary Institute for Basic Science (ICB) #
 # Faculty of Exact and Natural Sciences (FCEN)                                 #
 # Cuyo National University (UNCuyo), Mendoza, Argentina.                       #
